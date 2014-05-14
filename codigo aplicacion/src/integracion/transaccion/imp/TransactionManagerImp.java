05a5676506850bfb60e2a0028dd53fb8a4d97cc8
usuario_local <usuario_local@PTO0418>
2014-05-14 14:39:51 +0200

    el transaction manager nunca se queja de nada -> pues ahora YA SE QUEJA :)

--- codigo aplicacion/src/integracion/transaccion/imp/TransactionManagerImp.java
+++ codigo aplicacion/src/integracion/transaccion/imp/TransactionManagerImp.java
@@ -12,27 +12,64 @@ public class TransactionManagerImp extends TransactionManager {
 
 	private ConcurrentHashMap<Thread,Transaction> concurrentHashMap;
 
-	public TransactionManagerImp()
-	{
-		 concurrentHashMap = new ConcurrentHashMap<Thread,Transaction>();
+	public TransactionManagerImp() {
+		 
+		concurrentHashMap = new ConcurrentHashMap<Thread,Transaction>();
 
 	}
 	@Override
 	public void nuevaTransaccion() {
-		TransactionMySQL t = TransactionFactory.getInstance().crearTransactionMySQL();
-		concurrentHashMap.put(Thread.currentThread(), t);
+		
+		TransactionMySQL t;
+		
+		if(concurrentHashMap.containsKey(Thread.currentThread())) {
+			
+			t = (TransactionMySQL) concurrentHashMap.get(Thread.currentThread());
+			
+		} else {
+			
+			t = TransactionFactory.getInstance().crearTransactionMySQL();
+			concurrentHashMap.put(Thread.currentThread(), t);
+			
+		}
 		
 	}
 
 	@Override
 	public void eliminarTransaccion() {
-		concurrentHashMap.remove(Thread.currentThread());
+		
+		if(concurrentHashMap.containsKey(Thread.currentThread())) {
+			
+			TransactionMySQL t = (TransactionMySQL) concurrentHashMap.get(Thread.currentThread());
+			
+			try {
+				
+				t.commit();
+			
+			} catch (Exception e) {
+					
+				e.printStackTrace();
+			
+			}
+			
+			concurrentHashMap.remove(Thread.currentThread());
+			
+		}
+		
 	}
 
 	@Override
 	public Transaction getTransaction() {
-		return concurrentHashMap.get(Thread.currentThread());
+		
+		TransactionMySQL t = null ;
+		
+		if(concurrentHashMap.containsKey(Thread.currentThread())) {
+			
+			t = (TransactionMySQL) concurrentHashMap.get(Thread.currentThread());
+			
+		}
+		
+		return t;
 	}
-	
 	
 }
